24cbcbd40c957a0bf23515a39dac15f2d0d79be3
Srijit Bhowmick <143333173+gitprojects-Srijit@users.noreply.github.com>
2024-07-30 21:32:50 +0530

    Update HomeSlider.js

--- frontend/src/HomeSlider.js
+++ frontend/src/HomeSlider.js
@@ -4,7 +4,7 @@ function HomeSlider() {
            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                    <img class="d-block w-100" src="https://www.primedeq.com/blog/wp-content/uploads/2020/01/blood-bank.jpg" alt="First slide"/>
+                    <img class="d-block w-100" src="https://myshpl.com/website-assets/images/blood-donation.jpg" alt="First slide"/>
                     </div>
                 </div>
             </div>
